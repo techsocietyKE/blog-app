c00d8dc27a0edb6a623ebf42c19008e835fa7935
egreatsage <michaelcull9715@gmail.com>
2024-06-04 17:15:24 +0300

    sign-in complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/err.js'
+import jwt from 'jsonwebtoken'
 export const signup  = async (req,res,next)=>{
     const {username,email,password} = req.body
 
@@ -27,5 +28,36 @@ export const signup  = async (req,res,next)=>{
         res.json('Sign Up Successfull')
     } catch (error) {
         next(error);  
+    }
+}
+
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+
+    if(
+        !email ||!password || email === ''|| password ===''
+    ){
+        next(errorHandler(400,'All fields are required'))
+    }
+
+    try {
+
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            next(errorHandler(400,'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            next(errorHandler(400,'Invalid Password'))
+        }
+
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+            res.status(200).cookie('access_token',token,{
+                httpOnly:true
+            }).json(validUser)
+        
+    } catch (error) {
+        next(error)
     }
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  Spinner,
+} from '@chakra-ui/react'
 
 export default function SignIn() {
+  const [formData,setFormData] = useState({})
+  const [errorMessage,setErrorMessage] = useState(null)
+  const [loading,setLoading] = useState(false)
+
+  const navigate = useNavigate()
+    const handleChange = (e) =>{
+    setFormData({...formData,[e.target.id]: e.target.value});
+  }
+
+  const handleSubmit = async (ev) =>{
+    ev.preventDefault();
+    if ( !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all the fields")  
+    }
+    try {
+      setLoading(true)
+      setErrorMessage(null)
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json();
+
+      if (data.success === false){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false)
+    }
+  }
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex gap-5 p-3 max-w-3xl mx-auto flex-col md:items-center md:flex-row '>
+        <div className='flex-1'>
+        <Link className='flex whitespace-nowrap' to={'/'}>
+          <span className='text-6xl px-2 rounded-lg text-white bg-gradient-to-r from-blue-600 via-blue-500 to-pink-500'>Tech</span><h1 className='text-6xl'>Masters</h1>
+          </Link>
+        </div>
+        <div className='flex-1'>
+          <form onSubmit={handleSubmit}>
+            <div className="">
+              <label className="block text-gray-700 text-xl  mb-3" >Email</label>
+              <input
+              className='w-full my-1 border border-gray-400  py-1 outline-none rounded-md'
+              type='text'
+              placeholder='email'
+              id='email'
+              onChange={handleChange}
+              />
+            </div>
+            <div className="">
+              <label className="block text-gray-700 text-xl  mb-3" >Password</label>
+              <input
+              className='w-full my-1 border border-gray-400  py-1 outline-none rounded-md'
+              type='password'
+              placeholder='password'
+              id='password'
+              onChange={handleChange}
+              />
+            </div>
+            <div className='w-full flex'>
+         <button type='submit' className='text-xl font-semibold bg-gradient-to-r py-1 rounded-md mt-3 w-full text-white from-purple-700 to-pink-700'  disabled={loading}>
+        {
+          loading?(
+           <div className='flex items-center justify-center'>
+             <Spinner color='red.500' />
+            <span className='pl-3'>Loading...</span>
+           </div>
+          ):'Sign In'
+        }
+       </button>
+       </div>
+       <div className='my-2 gap-3 flex text-xl'>
+        <span>Dont have an account</span>
+        <Link className='text-[blue]' to='/sign-up'>Sign Up</Link>
+       </div>
+          </form>
+          <button type='submit' className='text-xl text-black hover:text-white font-semibold hover:bg-gradient-to-r py-1 rounded-md mt-3 w-full border border-purple-400 from-purple-700 to-pink-700' >
+          Continue with Google
+          </button>
+          {
+         errorMessage &&(
+          <Alert status='error'>
+          <AlertIcon />
+          <AlertDescription> {errorMessage}</AlertDescription>
+        </Alert>
+         )
+        }
+        </div>
+      </div>
+    </div>
   )
 }
