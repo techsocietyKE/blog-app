5d7ac793b0092d7962ccdf222e8d24b5fe9e8b98
egreatsage <michaelcull9715@gmail.com>
2024-06-05 14:40:45 +0300

    redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store,persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PersistGate persistor={persistor}>
       <Provider store={store}>
      <ChakraProvider>
          <App />
     </ChakraProvider>
     </Provider>
+    </PersistGate>
  
   </React.StrictMode>,
 )
